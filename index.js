38a7fd074b2e088138afc0d2fc0d998af4165759
toure <tourekadiy@gmail.com>
2017-08-22 12:11:06 +0200

    modification function fetch();

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function fetch(url) {
         xhr.open("GET", url);
         xhr.onload = function() {
             if (xhr.status == 200) {
-                ok(xhr.response);
+                ok(xhr.responseText);
             } else {
                 ko(xhr.status)
             }
